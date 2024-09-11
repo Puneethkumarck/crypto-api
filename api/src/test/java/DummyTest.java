b2fd0f0c2eca52b0ef0f8abf2c8a38ddb5154ee3
PuneethKumar Ck <punith.564@gmail.com>
2024-09-11 19:48:46 +0200

    fix sonar coverage

--- api/src/test/java/DummyTest.java
+++ api/src/test/java/DummyTest.java
@@ -2,9 +2,9 @@ import org.junit.jupiter.api.Test;
 
 public class DummyTest {
 
-   // TODO: REMOVE ME
-    @Test
-    void test() {
-        System.out.println("Hello World");
-    }
+  // TODO: REMOVE ME
+  @Test
+  void test() {
+    System.out.println("Hello World");
+  }
 }
