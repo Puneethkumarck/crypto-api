f9a233ca6fd072a9c933a77bd7d90f354177faa1
PuneethKumar Ck <punith.564@gmail.com>
2024-09-11 12:19:41 +0200

    Initial commit project skeleton

---
+++ app/src/test/java/com/blockchain/api/CryptoApiApplicationTests.java
@@ -0,0 +1,11 @@
+package com.blockchain.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoApiApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
