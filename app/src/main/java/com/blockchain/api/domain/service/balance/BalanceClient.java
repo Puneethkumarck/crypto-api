481d2d4a18744ac4fda0585585ac624d608bb0df
PuneethKumar Ck <punith.564@gmail.com>
2024-09-12 13:08:38 +0200

    additional tests

--- app/src/main/java/com/blockchain/api/domain/service/balance/BalanceClient.java
+++ app/src/main/java/com/blockchain/api/domain/service/balance/BalanceClient.java
@@ -7,6 +7,4 @@ public interface BalanceClient {
   CompletableFuture<BigDecimal> getBalance(String address, boolean isLamports);
 
   CompletableFuture<BigDecimal> getMinimumBalanceForRentExemption();
-
-  CompletableFuture<BigDecimal> getBalanceLamports(String address);
 }
